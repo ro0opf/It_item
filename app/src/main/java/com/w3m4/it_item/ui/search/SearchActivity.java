7844e0d7c7a7c48792f8e60001b5bd2983e14d64
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-05-20 14:29:55 +0900

    refeactoring

--- app/src/main/java/com/w3m4/it_item/ui/search/SearchActivity.java
+++ app/src/main/java/com/w3m4/it_item/ui/search/SearchActivity.java
@@ -1,25 +1,26 @@
 package com.w3m4.it_item.ui.search;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.w3m4.it_item.R;
-import com.w3m4.it_item.data.model.Photo;
+import com.w3m4.it_item.data.Photo;
 import com.w3m4.it_item.databinding.ActivitySearchBinding;
 
 import java.util.ArrayList;
 
-public class SearchActivity extends AppCompatActivity{
+public class SearchActivity extends AppCompatActivity {
     private ActivitySearchBinding binding;
     private ArrayList<Photo> photoArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
         initData();
     }
 
@@ -31,10 +32,10 @@ public class SearchActivity extends AppCompatActivity{
         photoArrayList.add(new Photo(R.drawable.kyusu_main, "큐슈"));
         photoArrayList.add(new Photo(R.drawable.kyoto_main, "교토"));
 
-        SearchAdapter adapter = new SearchAdapter(photoArrayList, this);
+        SearchAdapter adapter = new SearchAdapter(photoArrayList);
         GridLayoutManager manager = new GridLayoutManager(this, 2);
 
-        binding.recyclerView.setAdapter(adapter);
-        binding.recyclerView.setLayoutManager(manager);
+        binding.rcv.setAdapter(adapter);
+        binding.rcv.setLayoutManager(manager);
     }
 }
--- app/src/main/java/com/w3m4/it_item/ui/search/SearchAdapter.java
+++ app/src/main/java/com/w3m4/it_item/ui/search/SearchAdapter.java
@@ -1,34 +1,35 @@
 package com.w3m4.it_item.ui.search;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
-import com.w3m4.it_item.R;
 import com.w3m4.it_item.common.BaseRecyclerViewAdapter;
-import com.w3m4.it_item.data.model.Photo;
+import com.w3m4.it_item.data.Photo;
+import com.w3m4.it_item.databinding.ItemSearchBinding;
 
 import java.util.List;
 
-public class SearchAdapter extends BaseRecyclerViewAdapter<Photo,SearchAdapter.ViewHolder> {
-    Context context;
-    public SearchAdapter(List<Photo> dataSet, Context context) {
+public class SearchAdapter extends BaseRecyclerViewAdapter<Photo, SearchAdapter.ViewHolder> {
+    RequestManager requestManager;
+
+    public SearchAdapter(List<Photo> dataSet) {
         super(dataSet);
-        this.context = context;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemSearchBinding binding = ItemSearchBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        requestManager = Glide.with(parent.getContext());
+        return new ViewHolder(binding);
     }
 
     @Override
@@ -38,22 +39,19 @@ public class SearchAdapter extends BaseRecyclerViewAdapter<Photo,SearchAdapter.V
         RequestOptions options = new RequestOptions();
         options.circleCrop();
 
-        Glide.with(context)
-                .load(photo.getImage())
+        requestManager.load(photo.getImage())
                 .apply(options)
-                .into(holder.image);
-        holder.name.setText(photo.getName());
+                .into(holder.binding.ivImage);
+        holder.binding.tvName.setText(photo.getName());
     }
 
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
-        ImageView image;
-        TextView name;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ItemSearchBinding binding;
 
-        ViewHolder(View itemView) {
-            super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.imageView);
-            name = (TextView) itemView.findViewById(R.id.imageName);
+        ViewHolder(ItemSearchBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 }

5429a23b8f2f9220de1d85d92deed110d1897cd2
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-05-20 09:42:47 +0900

    Motion Layout 설정

--- app/src/main/java/com/w3m4/it_item/ui/MainActivity.java
+++ app/src/main/java/com/w3m4/it_item/ui/MainActivity.java
@@ -1,15 +1,37 @@
 package com.w3m4.it_item.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.w3m4.it_item.R;
+import com.w3m4.it_item.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private boolean flag = false;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        initMotionLayout();
+    }
+
+    void initMotionLayout(){
+        binding.nsvMain.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            if(scrollY > 1000 && !flag){
+                binding.mlMain.transitionToEnd();
+                flag = true;
+            }
+            if(scrollY < 1000 && flag){
+                binding.mlMain.transitionToStart();
+                flag = false;
+            }
+        });
     }
 }

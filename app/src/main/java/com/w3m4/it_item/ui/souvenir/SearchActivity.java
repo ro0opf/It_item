97e029b4bef1b29ca0b1d6576c71d2c413c294bd
sm5duck <sm5duck5@gmail.com>
2019-05-21 17:14:36 +0900

    Souvenir Search RecyclerView

--- app/src/main/java/com/w3m4/it_item/ui/souvenir/SearchActivity.java
+++ app/src/main/java/com/w3m4/it_item/ui/souvenir/SearchActivity.java
@@ -1,7 +1,6 @@
-package com.w3m4.it_item.ui.search;
+package com.w3m4.it_item.ui.souvenir;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +10,15 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import com.w3m4.it_item.R;
 import com.w3m4.it_item.data.Photo;
 import com.w3m4.it_item.databinding.ActivitySearchBinding;
+import com.w3m4.it_item.ui.search.SearchAdapter;
 
 import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
     private ActivitySearchBinding binding;
     private ArrayList<Photo> photoArrayList = new ArrayList<>();
+
     private EditText edtSearch;
 
     @Override
@@ -25,18 +27,18 @@ public class SearchActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
 
         edtSearch=(EditText)findViewById(R.id.edt_search);
-        edtSearch.setHint("여행할 도시를 선택해주세요.");
+        edtSearch.setHint("구매할 기념품을 선택해주세요.");
 
         initData();
     }
 
     private void initData() {
-        photoArrayList.add(new Photo(R.drawable.tokyo_main, "도쿄"));
-        photoArrayList.add(new Photo(R.drawable.osaka_main, "오사카"));
-        photoArrayList.add(new Photo(R.drawable.sapporo_main, "삿포로"));
-        photoArrayList.add(new Photo(R.drawable.hokkaido_main, "홋카이도"));
-        photoArrayList.add(new Photo(R.drawable.kyusu_main, "큐슈"));
-        photoArrayList.add(new Photo(R.drawable.kyoto_main, "교토"));
+        photoArrayList.add(new Photo(R.drawable.ic_food, "식품"));
+        photoArrayList.add(new Photo(R.drawable.ic_cosmetics, "뷰티"));
+        photoArrayList.add(new Photo(R.drawable.ic_pharmacy, "약품"));
+        photoArrayList.add(new Photo(R.drawable.ic_snack, "과자"));
+        photoArrayList.add(new Photo(R.drawable.ic_cat, "지역 특산품"));
+        photoArrayList.add(new Photo(R.drawable.ic_robot, "장난감"));
 
         SearchAdapter adapter = new SearchAdapter(photoArrayList);
         GridLayoutManager manager = new GridLayoutManager(this, 2);
@@ -44,4 +46,6 @@ public class SearchActivity extends AppCompatActivity {
         binding.rcv.setAdapter(adapter);
         binding.rcv.setLayoutManager(manager);
     }
+
+
 }

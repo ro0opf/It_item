96ff0a773029acedbe5b729bf3f61f73d9c2da94
ro0opf <34717634+ro0opf@users.noreply.github.com>
2019-05-22 14:47:51 +0900

    up

--- app/src/main/java/com/w3m4/it_item/ui/main/MainActivity.java
+++ app/src/main/java/com/w3m4/it_item/ui/main/MainActivity.java
@@ -1,16 +1,21 @@
 package com.w3m4.it_item.ui.main;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionScene;
 import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.transition.Transition;
 
 import com.w3m4.it_item.R;
+import com.w3m4.it_item.data.City;
 import com.w3m4.it_item.data.mLists;
 import com.w3m4.it_item.data.model.list.ListRepo;
 import com.w3m4.it_item.databinding.ActivityMainBinding;
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private CompositeDisposable disposable = new CompositeDisposable();
     private ListAdapter listAdapter;
+    private CityAdapter cityAdapter;
     private boolean flag = false;
 
     @Override
@@ -32,7 +38,40 @@ public class MainActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initMotionLayout();
         initListRcv(binding.rcvList);
+        initCityRcv(binding.rcvCity);
+        initClickListener();
+        fetchListData();
         fetchCityData();
+    }
+
+    private void fetchCityData() {
+        ArrayList<City> cityArrayList = new ArrayList<>();
+        cityArrayList.add(new City(R.drawable.tokyo_main, "Dummy"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "OSAKA"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "TOKYO"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "HOKKAIDO"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "KYUSHU"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "OKINAWA"));
+        cityArrayList.add(new City(R.drawable.tokyo_main, "KYOTO"));
+        cityAdapter.setItems(cityArrayList);
+    }
+
+    private void initCityRcv(RecyclerView rcv) {
+        cityAdapter = new CityAdapter(new ArrayList<>());
+        rcv.setHasFixedSize(true);
+        rcv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        rcv.setAdapter(cityAdapter);
+    }
+
+    private void initClickListener() {
+        binding.vCity.setOnClickListener(v -> {
+            binding.vItem2.setVisibility(View.INVISIBLE);
+            binding.vCity.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorWhite)));
+        });
+        binding.vItem.setOnClickListener(v -> {
+            binding.vItem2.setVisibility(View.VISIBLE);
+            binding.vCity.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorFaintMain)));
+        });
     }
 
     private void initListRcv(RecyclerView rcv) {
@@ -42,11 +81,11 @@ public class MainActivity extends AppCompatActivity {
         rcv.setAdapter(listAdapter);
     }
 
-    private void fetchCityData() {
+    private void fetchListData() {
         disposable.add(ListRepo.getInstance().getList("hyyyyjin")
-                .subscribe(data->{
+                .subscribe(data -> {
                             listAdapter.setItems(data.getLists());
-                        }, e->{
+                        }, e -> {
                             e.printStackTrace();
                         }
                 )
@@ -66,4 +105,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        disposable.dispose();
+        super.onDestroy();
+    }
 }
